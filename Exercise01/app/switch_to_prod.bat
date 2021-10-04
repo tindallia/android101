deb5c8b55206f9885e6b10c810fcde744033cadf
tindallia <ddnn_@outlook.com>
2021-10-04 23:28:51 +0700

    add workspace changer

---
+++ Exercise01/app/switch_to_prod.bat
@@ -0,0 +1,2 @@
+ren google-services.json google-services-test.json
+ren google-services-prod.json google-services.json
---
+++ Exercise01/app/switch_to_test.bat
@@ -0,0 +1,2 @@
+ren google-services.json google-services-prod.json
+ren google-services-test.json google-services.json
